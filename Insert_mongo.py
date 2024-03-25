550e9e6d9957fe2422695f23f6aa307883ba1684
Kris <kehayovkris>
2024-03-25 13:41:55 +0100

    V 4.0- OOP

--- Insert_mongo.py
+++ Insert_mongo.py
@@ -1,64 +1,64 @@
 
 from pymongo.server_api import ServerApi
 
-uri = "mongodb+srv://SuperAdmin:SuperAdmin123@interfaccia.zekgqcl.mongodb.net/?retryWrites=true&w=majority&appName=Interfaccia"
 
-# Create a new client and connect to the server
-client = MongoClient(uri, server_api=ServerApi('1'))
-db=client["Interfaccia"]
+class TesterMmongo:
+    def constructor(self):
+        uri = "mongodb+srv://SuperAdmin:SuperAdmin123@interfaccia.zekgqcl.mongodb.net/?retryWrites=true&w=majority&appName=Interfaccia"
+        # Create a new client and connect to the server
+        self.client = MongoClient(uri, server_api=ServerApi('1'))
+        self.db=self.client["Interfaccia"]
+        # Send a ping to confirm a successful connection
+        try:
+            self.client.admin.command('ping')
+            print("Pinged your deployment. You successfully connected to MongoDB!")
+        except Exception as e:
+            print(e)
 
-# Send a ping to confirm a successful connection
-try:
-    client.admin.command('ping')
-    print("Pinged your deployment. You successfully connected to MongoDB!")
-except Exception as e:
-    print(e)
-
-
-def insert_mappa(id, lunghezza,larghezza, prezzo):
-    collection=db["Mappa"]
-    doc={"id":id,"lunghezza":lunghezza,"larghezza":larghezza,"prezzo":prezzo}
-    result=collection.insert_one(doc)
-    if result.acknowledged:
-        print("Documento inserito correttamente")
-    else:
-        print("Errore durante l'inserimento del documento")
-
-
-def insert_casa(id, lunghezza, larghezza, piani, appartamenti, tipologia, prezzo ):
-    collection=db["Casa"]
-    doc={"id":id,"lunghezza":lunghezza,"larghezza":larghezza,"piani":piani,"appartamenti":appartamenti,"tipologia":tipologia,"prezzo":prezzo}
-    result=collection.insert_one(doc)
-    if result.acknowledged:
-        print("Documento inserito correttamente")
-    else:
-        print("Errore durante l'inserimento del documento")
+    def insert_mappa(self,id, lunghezza,larghezza, prezzo):
+        collection=self.db["Mappa"]
+        doc={"id":id,"lunghezza":lunghezza,"larghezza":larghezza,"prezzo":prezzo}
+        result=collection.insert_one(doc)
+        if result.acknowledged:
+            print("Documento inserito correttamente")
+        else:
+            print("Errore durante l'inserimento del documento")
 
 
-def insert_negozio(id, lunghezza, larghezza, nome_attività, servizi, prezzo):
-    collection=db["Casa"]
-    doc={"id":id,"lunghezza":lunghezza,"larghezza":larghezza,"nome_attivita":nome_attività,"servizi":servizi,"prezzo":prezzo}
-    result=collection.insert_one(doc)
-    if result.acknowledged:
-        print("Documento inserito correttamente")
-    else:
-        print("Errore durante l'inserimento del documento")
+    def insert_casa(self,id, lunghezza, larghezza, piani, appartamenti, tipologia, prezzo ):
+        collection=self.db["Casa"]
+        doc={"id":id,"lunghezza":lunghezza,"larghezza":larghezza,"piani":piani,"appartamenti":appartamenti,"tipologia":tipologia,"prezzo":prezzo}
+        result=collection.insert_one(doc)
+        if result.acknowledged:
+            print("Documento inserito correttamente")
+        else:
+            print("Errore durante l'inserimento del documento")
 
 
-def insert_EdificioP(id, lunghezza, larghezza, funzione, tipologia, prezzo, id_negozio ):
-    collection=db["EdificioPubblico"]
-    doc={"id":id,"lunghezza":lunghezza,"larghezza":larghezza,"funzione":funzione,"tipologia":tipologia,"prezzo":prezzo,"id_negozio":id_negozio}
-    result=collection.insert_one(doc)
-    if result.acknowledged:
-        print("Documento inserito correttamente")
-    else:
-        print("Errore durante l'inserimento del documento")
+    def insert_negozio(self,id, lunghezza, larghezza, nome_attività, servizi, prezzo):
+        collection=self.db["Casa"]
+        doc={"id":id,"lunghezza":lunghezza,"larghezza":larghezza,"nome_attivita":nome_attività,"servizi":servizi,"prezzo":prezzo}
+        result=collection.insert_one(doc)
+        if result.acknowledged:
+            print("Documento inserito correttamente")
+        else:
+            print("Errore durante l'inserimento del documento")
 
-def SpazioPubblico(id, lunghezza, larghezza, funzione, prezzo):
-    collection=db["EdificioPubblico"]
-    doc={"id":id,"lunghezza":lunghezza,"larghezza":larghezza,"funzione":funzione,"prezzo":prezzo}
-    result=collection.insert_one(doc)
-    if result.acknowledged:
-        print("Documento inserito correttamente")
-    else:
-        print("Errore durante l'inserimento del documento")
+
+    def insert_EdificioP(self,id, lunghezza, larghezza, funzione, tipologia, prezzo, id_negozio ):
+        collection=self.db["EdificioPubblico"]
+        doc={"id":id,"lunghezza":lunghezza,"larghezza":larghezza,"funzione":funzione,"tipologia":tipologia,"prezzo":prezzo,"id_negozio":id_negozio}
+        result=collection.insert_one(doc)
+        if result.acknowledged:
+            print("Documento inserito correttamente")
+        else:
+            print("Errore durante l'inserimento del documento")
+
+    def SpazioPubblico(self,id, lunghezza, larghezza, funzione, prezzo):
+        collection=self.db["EdificioPubblico"]
+        doc={"id":id,"lunghezza":lunghezza,"larghezza":larghezza,"funzione":funzione,"prezzo":prezzo}
+        result=collection.insert_one(doc)
+        if result.acknowledged:
+            print("Documento inserito correttamente")
+        else:
+            print("Errore durante l'inserimento del documento")
--- import mysql.py
+++ import mysql.py
@@ -1,96 +1,98 @@
 import pymssql
 
-connesione=pymssql.connect(
-            server='ce2.database.windows.net',
-            user='ApiUser',
-            password='ApiTeam123',
-            database='CE')
 
-cursore=connesione.cursor() #Gestione chiusura connesione e ecursore MANCANTE
+class TesterSql:
+    def constructor(self):
+        self.connesione=pymssql.connect(
+                    server='ce2.database.windows.net',
+                    user='ApiUser',
+                    password='ApiTeam123',
+                    database='CE')
+        self.cursore=self.connesione.cursor() #Gestione chiusura connesione e ecursore MANCANTE
 
-def insert_Utente(username,nome,cognome,data,email,password,citta):
-    query="INSERT INTO Utente(Username,Nome,Cognome,data_nascita,email,password,citta) VALUES(?,?,?,?,?,?,?)"
-    res=cursore.execute(query,(username,nome,cognome,data,email,password,citta))
-    connesione.commit()
-    if res== True:
-        print("OPERAZIONE AVVENUTA CON SUCCESSO")
-    else:
-        print("ERRORE")
-    
-def insert_Simulazione(Id_Simulazione, data_creazione, nome_città, UsernameUtente, MeteoDT):
-    query="INSERT INTO Simulazione(Id_Simulazione, data_creazione, nome_città, Utente, Meteo) VALUES(?,?,?,?,?,?,?)"
-    res=cursore.execute(query,(Id_Simulazione, data_creazione, nome_città, UsernameUtente, MeteoDT))
-    connesione.commit()
-    if res== True:
-        print("OPERAZIONE AVVENUTA CON SUCCESSO")
-    else:
-        print("ERRORE")
-
-
-def insert_Meteo(Datatime, pressione, temperatura, velocità_vento, direzione_vento, umidità, morfologia, precipitazioni, irraggiamento):
-    query="INSERT INTO Meteo(Datatime, pressione, temperatura, velocità_vento, direzione_vento, umidità, morfologia, precipitazioni, irraggiamento) VALUES(?,?,?,?,?,?,?)"
-    res=cursore.execute(query,(Datatime, pressione, temperatura, velocità_vento, direzione_vento, umidità, morfologia, precipitazioni, irraggiamento))
-    connesione.commit()
-    if res== True:
-        print("OPERAZIONE AVVENUTA CON SUCCESSO")
-    else:
-        print("ERRORE")
-
-def insert_Batteria(Id_Serie, energia_stoccata, entrata, uscita, datatime, capacità_max, Id_sim):
-    query="INSERT INTO Batteria(Id_Serie, energia_stoccata, entrata, uscita, datatime, capacità_max, Id_sim) VALUES(?,?,?,?,?,?,?)"
-    res=cursore.execute(query,(Id_Serie, energia_stoccata, entrata, uscita, datatime, capacità_max, Id_sim))
-    connesione.commit()
-    if res== True:
-        print("OPERAZIONE AVVENUTA CON SUCCESSO")
-    else:
-        print("ERRORE")
-
-def insert_Compravendita(Id_Transazione, datatime, guadagno, quantità_venduta, spesa, quantità_comprata, IdBatteria):
-    query="INSERT INTO Compravendita(Id_Transazione, datatime, guadagno, quantità_venduta, spesa, quantità_comprata, IdBatteria) VALUES(?,?,?,?,?,?,?)"
-    res=cursore.execute(query,(Id_Transazione, datatime, guadagno, quantità_venduta, spesa, quantità_comprata, IdBatteria))
-    connesione.commit()
-    if res== True:
-        print("OPERAZIONE AVVENUTA CON SUCCESSO")
-    else:
-        print("ERRORE")
+    def insert_Utente(self,username,nome,cognome,data,email,password,citta):
+        query="INSERT INTO Utente(Username,Nome,Cognome,data_nascita,email,password,citta) VALUES(?,?,?,?,?,?,?)"
+        res=self.cursore.execute(query,(username,nome,cognome,data,email,password,citta))
+        self.connesione.commit()
+        if res== True:
+            print("OPERAZIONE AVVENUTA CON SUCCESSO")
+        else:
+            print("ERRORE")
+        
+    def insert_Simulazione(self,Id_Simulazione, data_creazione, nome_città, UsernameUtente, MeteoDT):
+        query="INSERT INTO Simulazione(Id_Simulazione, data_creazione, nome_città, Utente, Meteo) VALUES(?,?,?,?,?,?,?)"
+        res=self.cursore.execute(query,(Id_Simulazione, data_creazione, nome_città, UsernameUtente, MeteoDT))
+        self.connesione.commit()
+        if res== True:
+            print("OPERAZIONE AVVENUTA CON SUCCESSO")
+        else:
+            print("ERRORE")
 
 
-def insert_Struttura(Id_Edificio, consumo, posizione, direzione, costo, numero_pannelli, numero_abitanti, piani, superficie, tipo, IdSimulazione, IdPannello, IdProduzione):
-    query="INSERT INTO Struttura(Id_Edificio, consumo, posizione, direzione, costo, numero_pannelli, numero_abitanti, piani, superficie, tipo, IdSimulazione, IdPannello, IdProduzione) VALUES(?,?,?,?,?,?,?)"
-    res=cursore.execute(query,(Id_Edificio, consumo, posizione, direzione, costo, numero_pannelli, numero_abitanti, piani, superficie, tipo, IdSimulazione, IdPannello, IdProduzione))
-    connesione.commit()
-    if res== True:
-        print("OPERAZIONE AVVENUTA CON SUCCESSO")
-    else:
-        print("ERRORE")
+    def insert_Meteo(self,Datatime, pressione, temperatura, velocità_vento, direzione_vento, umidità, morfologia, precipitazioni, irraggiamento):
+        query="INSERT INTO Meteo(Datatime, pressione, temperatura, velocità_vento, direzione_vento, umidità, morfologia, precipitazioni, irraggiamento) VALUES(?,?,?,?,?,?,?)"
+        res=self.cursore.execute(query,(Datatime, pressione, temperatura, velocità_vento, direzione_vento, umidità, morfologia, precipitazioni, irraggiamento))
+        self.connesione.commit()
+        if res== True:
+            print("OPERAZIONE AVVENUTA CON SUCCESSO")
+        else:
+            print("ERRORE")
 
-def insert_TipoPannello(Id_pannello, Dimensioni, Stato, Prezzo):
-    query="INSERT INTO TipoPannello(Id_pannello, Dimensioni, Stato, Prezzo) VALUES(?,?,?,?,?,?,?)"
-    res=cursore.execute(query,(Id_pannello, Dimensioni, Stato, Prezzo))
-    connesione.commit()
-    if res== True:
-        print("OPERAZIONE AVVENUTA CON SUCCESSO")
-    else:
-        print("ERRORE")
+    def insert_Batteria(self,Id_Serie, energia_stoccata, entrata, uscita, datatime, capacità_max, Id_sim):
+        query="INSERT INTO Batteria(Id_Serie, energia_stoccata, entrata, uscita, datatime, capacità_max, Id_sim) VALUES(?,?,?,?,?,?,?)"
+        res=self.cursore.execute(query,(Id_Serie, energia_stoccata, entrata, uscita, datatime, capacità_max, Id_sim))
+        self.connesione.commit()
+        if res== True:
+            print("OPERAZIONE AVVENUTA CON SUCCESSO")
+        else:
+            print("ERRORE")
 
-def insert_ProduzioneStorico(Id_produzione,Potenza,Datatime):
-    query="INSERT INTO ProduzioneStorico(Id_produzione,Potenza,Datatime) VALUES(?,?,?,?,?,?,?)"
-    res=cursore.execute(query,(Id_produzione,Potenza,Datatime))
-    connesione.commit()
-    if res== True:
-        print("OPERAZIONE AVVENUTA CON SUCCESSO")
-    else:
-        print("ERRORE")
-
-def insert_CostoEnergiaStorico(Id_costo,Datatime,Costo_acquisto,Costo_vendita):
-    query="INSERT INTO Costo_Energia_Storico(Id_costo,Datatime,Costo_acquisto,Costo_vendita) VALUES(?,?,?,?,?,?,?)"
-    res=cursore.execute(query,(Id_costo,Datatime,Costo_acquisto,Costo_vendita))
-    connesione.commit()
-    if res== True:
-        print("OPERAZIONE AVVENUTA CON SUCCESSO")
-    else:
-        print("ERRORE")
+    def insert_Compravendita(self,Id_Transazione, datatime, guadagno, quantità_venduta, spesa, quantità_comprata, IdBatteria):
+        query="INSERT INTO Compravendita(Id_Transazione, datatime, guadagno, quantità_venduta, spesa, quantità_comprata, IdBatteria) VALUES(?,?,?,?,?,?,?)"
+        res=self.cursore.execute(query,(Id_Transazione, datatime, guadagno, quantità_venduta, spesa, quantità_comprata, IdBatteria))
+        self.connesione.commit()
+        if res== True:
+            print("OPERAZIONE AVVENUTA CON SUCCESSO")
+        else:
+            print("ERRORE")
 
 
+    def insert_Struttura(self,Id_Edificio, consumo, posizione, direzione, costo, numero_pannelli, numero_abitanti, piani, superficie, tipo, IdSimulazione, IdPannello, IdProduzione):
+        query="INSERT INTO Struttura(Id_Edificio, consumo, posizione, direzione, costo, numero_pannelli, numero_abitanti, piani, superficie, tipo, IdSimulazione, IdPannello, IdProduzione) VALUES(?,?,?,?,?,?,?)"
+        res=self.cursore.execute(query,(Id_Edificio, consumo, posizione, direzione, costo, numero_pannelli, numero_abitanti, piani, superficie, tipo, IdSimulazione, IdPannello, IdProduzione))
+        self.connesione.commit()
+        if res== True:
+            print("OPERAZIONE AVVENUTA CON SUCCESSO")
+        else:
+            print("ERRORE")
 
-##PROVARE LE FUNZIONI 
+    def insert_TipoPannello(self,Id_pannello, Dimensioni, Stato, Prezzo):
+        query="INSERT INTO TipoPannello(Id_pannello, Dimensioni, Stato, Prezzo) VALUES(?,?,?,?,?,?,?)"
+        res=self.cursore.execute(query,(Id_pannello, Dimensioni, Stato, Prezzo))
+        self.connesione.commit()
+        if res== True:
+            print("OPERAZIONE AVVENUTA CON SUCCESSO")
+        else:
+            print("ERRORE")
+
+    def insert_ProduzioneStorico(self,Id_produzione,Potenza,Datatime):
+        query="INSERT INTO ProduzioneStorico(Id_produzione,Potenza,Datatime) VALUES(?,?,?,?,?,?,?)"
+        res=self.cursore.execute(query,(Id_produzione,Potenza,Datatime))
+        self.connesione.commit()
+        if res== True:
+            print("OPERAZIONE AVVENUTA CON SUCCESSO")
+        else:
+            print("ERRORE")
+
+    def insert_CostoEnergiaStorico(self,Id_costo,Datatime,Costo_acquisto,Costo_vendita):
+        query="INSERT INTO Costo_Energia_Storico(Id_costo,Datatime,Costo_acquisto,Costo_vendita) VALUES(?,?,?,?,?,?,?)"
+        res=self.cursore.execute(query,(Id_costo,Datatime,Costo_acquisto,Costo_vendita))
+        self.connesione.commit()
+        if res== True:
+            print("OPERAZIONE AVVENUTA CON SUCCESSO")
+        else:
+            print("ERRORE")
+
+
+
+    ##PROVARE LE FUNZIONI 
